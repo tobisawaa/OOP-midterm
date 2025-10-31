11d57dbbf60cb92a5b7d0ce44bc5633bb54c29d7
yukiraa <93199720+tobisawaa@users.noreply.github.com>
2025-10-31 15:03:44 +0700

    Add files via upload

---
+++ luaslingkaran constructor&destructor.php
@@ -0,0 +1,27 @@
+<?php
+
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+
+    public function __construct($IsiJari = 1){
+        $this->jari = $IsiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+    $rumus = LuasLingkaran:: phi * $this->jari * $this->jari;
+    echo "Lingkaran {$nama} Hasilnya adalah: {$rumus}";
+}
+ public static function testing() {
+    echo "<br/>";
+    echo "Ini dari static";
+ }
+
+ public function __destruct() {
+    echo "<br/>";
+    echo "malasnyo";
+ }
+} 
+$Lingkaran = new LuasLingkaran(7);
+$Lingkaran->tampil('ban'); // Memanggil metode tampil dari objek $Lingkaran
+LuasLingkaran::testing();
